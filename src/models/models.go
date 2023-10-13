266b1fe24b625d263279a44f91de5eca852ea2a2
kocierik <kocierik@gmail.com>
2023-10-13 15:49:42 +0200

    fix: ignore

--- src/models/models.go
+++ src/models/models.go
@@ -1,9 +1,11 @@
 package models
 
 type Music struct {
-	Id     int    `json:"id" gorm:"primaryKey"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	Desc   string `json:"desc"`
-	// Duration int    `json:"duration"`
+	Id       int    `json:"id" gorm:"primaryKey"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Desc     string `json:"desc"`
+	Format   string `json:"format"`
+	Duration int    `json:"duration"`
+	Path     string `json:"path"`
 }
