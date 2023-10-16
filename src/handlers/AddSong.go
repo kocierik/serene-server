e57631723b0ebf3c5b9a3e86a513c7225ff5b859
kocierik <kocierik@gmail.com>
2023-10-16 13:31:44 +0200

    feat: add songs

--- src/handlers/AddSong.go
+++ src/handlers/AddSong.go
@@ -3,27 +3,43 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
 
+	"github.com/dhowden/tag"
 	"github.com/kocierik/SwiftServe/src/models"
+	"github.com/kocierik/SwiftServe/src/utils"
 )
 
 func (h handler) AddSong(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	body, err := io.ReadAll(r.Body)
-
+	mp3File, _, err := r.FormFile("song")
 	if err != nil {
-		log.Fatalln(err)
+		http.Error(w, "Failed to read file", http.StatusBadRequest)
+		return
 	}
-	var song models.Music
-	json.Unmarshal(body, &song)
+	defer mp3File.Close()
+
+	mp3Tag, err := tag.ReadFrom(mp3File)
+	if err != nil {
+		http.Error(w, "Failed to read MP3 tags", http.StatusBadRequest)
+		return
+	}
+	songDuration := utils.GetLengthSong(mp3File)
+
+	song := models.Music{
+		Title:    mp3Tag.Title(),
+		Artist:   mp3Tag.Artist(),
+		Album:    mp3Tag.Album(),
+		Duration: songDuration,
+		Picture:  mp3Tag.Picture().Data,
+	}
+
 	if result := h.DB.Create(&song); result.Error != nil {
-		fmt.Println(result.Error)
+		http.Error(w, "Failed to save metadata to the database", http.StatusInternalServerError)
+		return
 	}
-	w.Header().Add("Content-Type", "application/json")
+
+	fmt.Println("DONE: POST ADD SONG")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(song)
-
 }
